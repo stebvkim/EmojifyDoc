e7e56189604e9cdd190b2ac1f7cb431f7f03ad92
seok <seok@mit.edu>
2022-10-01 17:43:47 -0400

    added pytesseract bounding box information

--- EmojifyDoc.py
+++ EmojifyDoc.py
@@ -1,31 +1,52 @@
 # to be implemented.
 import os
+import pytesseract as pt
 from pdf2image import convert_from_path
+from PIL import Image
 
-def convert_and_save_as_image(pdf_pathname: str) -> list[str]:
-    ''' Takes an input PDF and creates a new folder containing the individual pages as JPG files. '''
+# Need this to make Pytesseract run on my computer. Will differ for other users.
+pt.pytesseract.tesseract_cmd = r'C:\Users\steve\AppData\Local\Tesseract-OCR\tesseract.exe'
 
+def pdf_to_pil_images(pdf_pathname: str) -> list:
+    ''' Takes an input PDF and returns a list of PIL images. '''
+
+    # Make sure we get valid inputs
     assert os.path.exists(pdf_pathname), "File does not exist."
     PDF_HANDLE_INDEX = -3
     assert pdf_pathname[PDF_HANDLE_INDEX:].lower() == 'pdf', "File is not a PDF."
 
     individual_images = convert_from_path(pdf_pathname)
-
-    path = os.path.join(os.getcwd(), 'test_file_storage')
-    if not os.path.isdir(path):
-        os.mkdir(path)
-
     im_paths = []
-
-    for ix, im in enumerate(individual_images):
-        im_path = os.path.join(path, f'page_{ix}.jpg')
-        im.save(im_path)
-        im_paths.append(im_path)
+    for path in individual_images:
+        im_paths.append(path)
 
     return im_paths
 
-def get_words_and_bounding_boxes(image_path: str):
-    pass
+def get_words_and_bounding_boxes(pil_image) -> dict:
+    ''' Takes a PIL image and returns a dict mapping each word to a list of tuples where that word appears. '''
+    # width, height = pil_image.size
+    # print(width, height)
+
+    # Keys: 'level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text'
+    img_dict = pt.image_to_data(pil_image, output_type = pt.Output.DICT)
+    formatted_words = {}
+    for ix, word in enumerate(img_dict['text']):
+        if word:
+            x1 = img_dict['left'][ix]
+            y1 = img_dict['top'][ix]
+            x2 = img_dict['left'][ix] + img_dict['width'][ix]
+            y2 = img_dict['top'][ix] + img_dict['height'][ix]
+            if word in formatted_words:
+                formatted_words[word].append((x1, y1, x2, y2))
+            else:
+                formatted_words[word] = [(x1, y1, x2, y2)]
+
+    return formatted_words
 
 # print(convert_and_save_as_image('test_file_storage'))
 # print(convert_and_save_as_image('test_file_storage\page_0.jpg'))
+
+# images = pdf_to_pil_images('ps1.pdf')
+# for im in images:
+#     print(get_words_and_bounding_boxes(im))
+#     break
